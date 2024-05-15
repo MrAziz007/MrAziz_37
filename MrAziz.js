e9945edaf74a52bb3df1e1ef36bf67d8b5d048b2
MrAziz007 <isido7094@gmail.com>
2024-05-15 18:09:22 +0500

    Циклы

---
+++ MrAziz.js
@@ -0,0 +1,20 @@
+// const color = ['red', 'green', 'blue', 'yellow'];
+// for (let index = 0; index < color.length; index++) {
+//     console.log(color[index]);   
+// };
+
+// let j = 1;
+// do {
+//     console.log(j**2); 
+//     j++;
+// } while (j <= 10); 
+
+// for (j = 1; j <= 10; j++) {
+//     console.log(j**2);
+// };
+
+// for (let index = 1; index <= 20; index++) {
+//     if (index%2 == 1) {
+//         console.log(index);
+//     }
+// };
